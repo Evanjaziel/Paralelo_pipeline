79dc1e6ccc1a0d9a9cfd7a0badf4f829f5c97a5c
Evan <evanjaziel@hotmail.com>
2024-08-02 08:38:14 -0600

    agregando screenshots

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'cz937d',
-  video: true,
+  video: false,
+  screenshots: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
